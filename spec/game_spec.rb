62f5a980de3cef459ab92781b1d8f140d1e82266
Cody Swartz <ctsstc@gmail.com>
2019-06-05 11:42:55 -0700

    Clean up game instantiation in tests

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,27 +1,25 @@
 require_relative '../lib/game'
 
 describe 'A game of rock paper scissors' do
+  let(:game) { Game.new }
 
   it 'exists' do
-    Game.new
+    game
   end
 
   it 'can start' do
-    Game.new.start
+    game.start
   end
 
   it 'is not initially started' do
-    game = Game.new
     expect(game).to_not be_started
   end
 
   it 'is not initially finished' do
-    game = Game.new
     expect(game.finished).to_not eq(true)
   end
 
   it 'a started game knows that it has started' do
-    game = Game.new
     game.start
     expect(game).to be_started
   end
@@ -30,32 +28,27 @@ describe 'A game of rock paper scissors' do
 
     context 'without starting' do
       it 'can not be played if it has not started' do
-        game = Game.new
         expect { game.play }.to raise_error(RuntimeError)
       end
     end
 
     context 'after starting' do
       it 'can be played if it has started' do
-        game = Game.new
         game.start
         expect { game.play }.to_not raise_error
       end
 
       it 'returns nil when not provided with anything' do
-        game = Game.new
         game.start
         expect(game.play).to be_nil
       end
 
       it 'returns nil when only provided one rock, paper or scissor' do
-        game = Game.new
         game.start
         expect(game.play(:FAKE)).to be_nil
       end
 
       it 'does not return nil when two objects are provided' do
-        game = Game.new
         game.start
         expect(game.play(:FAKE, :FAKE)).to_not be_nil
       end
@@ -63,14 +56,12 @@ describe 'A game of rock paper scissors' do
 
     context 'rock vs scissors' do
       it 'announces the correct winner' do
-        game = Game.new
         game.start
         expectation = 'Rock beats scissors!'
         expect(game.play(:rock, :scissors)).to eq(expectation)
         expect(game.play(:scissors, :rock)).to eq(expectation)
       end
       it 'has a finished game state' do
-        game = Game.new
         game.start
         game.play(:rock, :scissors)
         expect(game.finished).to eq(true)
@@ -79,14 +70,12 @@ describe 'A game of rock paper scissors' do
 
     context 'rock vs paper' do
       it 'announces the correct winner' do
-        game = Game.new
         game.start
         expectation = 'Paper beats rock!'
         expect(game.play(:paper, :rock)).to eq(expectation)
         expect(game.play(:rock, :paper)).to eq(expectation)
       end
       it 'has a finished game state' do
-        game = Game.new
         game.start
         game.play(:paper, :rock)
         expect(game.finished).to eq(true)
@@ -95,14 +84,12 @@ describe 'A game of rock paper scissors' do
 
     context 'scissors vs paper' do
       it 'announces the correct winner' do
-        game = Game.new
         game.start
         expectation = 'Scissors beats paper!'
         expect(game.play(:scissors, :paper)).to eq(expectation)
         expect(game.play(:paper, :scissors)).to eq(expectation)
       end
       it 'has a finished game state' do
-        game = Game.new
         game.start
         game.play(:scissors, :paper)
         expect(game.finished).to eq(true)
@@ -111,7 +98,6 @@ describe 'A game of rock paper scissors' do
 
     context 'a tie game' do
       it 'announces a tie game' do
-        game = Game.new
         game.start
         expectation = 'Tie game. Try again!'
         expect(game.play(:rock, :rock)).to eq(expectation)
@@ -120,7 +106,6 @@ describe 'A game of rock paper scissors' do
       end
 
       it 'has a finished game state' do
-        game = Game.new
         game.start
         game.play(:rock, :rock)
         expect(game.finished).to eq(false)
